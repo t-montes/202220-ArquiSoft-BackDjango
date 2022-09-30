ad3445df85bfada205a2ad878b600f6cd2f0c68c
Brian RIvera <b.riverah@uniandes.edu.co>
2022-09-30 17:55:24 -0500

    prueba

--- avanzo/avanzo/settings.py
+++ avanzo/avanzo/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'avanzo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
         'NAME': "avanzo",
         'USER': "metateoremas",
         'PASSWORD': "arquisoft2022",
