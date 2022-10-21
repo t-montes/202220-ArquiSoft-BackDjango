e05db82a22a87e72436019705c49b59a6d289462
Your Name <you@example.com>
2022-10-21 15:41:55 -0500

    docsfix5

--- documentos/views.py
+++ documentos/views.py
@@ -7,13 +7,11 @@ from django.http import HttpResponse
 
 
 def analizar_documentos(request):
-    print("request::::::::::::::", request)
     if request.method == 'POST':
         form = DocumentosForm(request.POST)
-        print("-----------", form)
         if form.is_valid():
             print("El form si es valido")
-            analizador_documentos(form)
+            analizador_documentos(request)
             messages.add_message(request, messages.SUCCESS, 'Successfully analizing the document')
             return HttpResponse('ok',status=200)
         else:
