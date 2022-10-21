6ab08bc6441f316abb0f938ea12c5f77bd102db1
Your Name <you@example.com>
2022-10-21 14:18:56 -0500

    Model forms fiz

--- documentos/forms.py
+++ documentos/forms.py
@@ -3,6 +3,15 @@ from .models import Documento
 
 class DocumentosForm(forms.ModelForm):
     class Meta:
+        model = Documento
+        fields = [
+            'path_image', 'num_documento', 'nombre'
+        ]
+        labels = {
+            'path_image': forms.CharField(max_length=100),
+            'num_documento': forms.CharField(max_length=100),
+            'nombre': forms.CharField(max_length=100)
+        }
         path_image = forms.CharField(max_length=100)
         num_documento = forms.CharField(max_length=100)
         nombre = forms.CharField(max_length=100)
