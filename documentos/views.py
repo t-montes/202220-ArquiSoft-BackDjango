2c4217e684745e319701e6cd592a43654e58372b
Your Name <you@example.com>
2022-10-21 14:33:30 -0500

    docimentos fix

--- documentos/views.py
+++ documentos/views.py
@@ -4,22 +4,25 @@ from django.contrib import messages
 from .forms import DocumentosForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.http import HttpResponse
+
 
 
 def analizar_documentos(request):
     if request.method == 'POST':
         form = DocumentosForm(request.POST)
         if form.is_valid():
+            print("El form si es valido")
             analizador_documentos(form)
             messages.add_message(request, messages.SUCCESS, 'Successfully analizing the document')
-            return HttpResponseRedirect(reverse('documentAnalize'))
+            return HttpResponse('ok')
         else:
             print(form.errors)
     else:
+        print("No es valido el form")
         form = DocumentosForm()
 
-    context = {
+        context = {
         'form': form,
-    }
-
-
+        }
+        return HttpResponse('not ok')
