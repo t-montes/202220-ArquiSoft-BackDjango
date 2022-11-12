28a95e2dd8e95036c5351e834329314509ed6f04
t-montes <t.montes@uniandes.edu.co>
2022-11-11 23:51:40 -0500

    change debug

--- creditos/views.py
+++ creditos/views.py
@@ -10,32 +10,22 @@ from django.http import QueryDict
 @login_required
 def credito_update(request):
 
-    print("Request recibida")
     role = getRole(request)
-    print("ROL:", role)
     if request.method == 'PUT':
-        print("Rol Válido")
-        print("Método: PUT")
         if role == "ANALIZADOR":
             print("request BODY", request.body)
-            for i in dir(request):
-                print("\t",i, getattr(request, i))
             form = CreditoForm(QueryDict(request.body))
             print("FORM [views]:", form)
             if form.is_valid():
-                print("FORM IS VALID [views]")
                 update_credit(form)
-                print("FORM UPDATED [views]")
                 messages.add_message(request, messages.SUCCESS, 'Credito actualizado correctamente')
                 return HttpResponse(status=200)
             else:
                 messages.add_message(request, messages.ERROR, 'Error al actualizar credito')
                 return HttpResponse(status=200)
         else:
-            print("Método: OTRO")
             return HttpResponse("Unauthorized User", status=401)
     else:
-        print("ROL INVÁLIDO")
         return HttpResponse("BAD REQUEST", status=204)
 
 # Create your views here.
