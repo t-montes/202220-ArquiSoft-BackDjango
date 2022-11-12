f91f013d1a18d5067eba3f2593e759b05713ce1e
Your Name <you@example.com>
2022-11-11 21:46:13 -0500

    fix

--- avanzo/settings.py
+++ avanzo/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'avanzo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'monitoring', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
