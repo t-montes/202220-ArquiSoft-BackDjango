dcd14dc5cb7711f6d656b48334e96acfa0d6eb55
Braito <b.riverah@uniandes.edu.co>
2022-11-17 14:45:02 -0500

    a

--- creditos/urls.py
+++ creditos/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from django.conf.urls import include
-
 from . import views
-
 urlpatterns = [
     path('creditoupdate/', csrf_exempt(views.credito_update), name='creditoUpdate'),
+    path('',views.esta_funcionando_papi(), name='estaFuncionandoPapi')
 ]
--- creditos/views.py
+++ creditos/views.py
@@ -11,14 +11,15 @@ import json
 def credito_update(request):
     # role = getRole(request)
     if request.method == 'PUT':
-            print("request BODY", request.body)
-            # request.body to dict
-            body = request.body.decode('utf-8')
-            body = json.loads(body)
-            update_credit(body)
-            messages.add_message(request, messages.SUCCESS, 'Credito actualizado correctamente')
-            return HttpResponse(status=200)
+        print("request BODY", request.body)
+        # request.body to dict
+        body = request.body.decode('utf-8')
+        body = json.loads(body)
+        update_credit(body)
+        messages.add_message(request, messages.SUCCESS, 'Credito actualizado correctamente')
+        return HttpResponse(status=200)
     else:
         return HttpResponse("BAD REQUEST", status=400)
-
+def esta_funcionando_papi(request):
+    return HttpResponse("Esta funcionando papi")
 # Create your views here.
