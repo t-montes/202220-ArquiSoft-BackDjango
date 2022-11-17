d3a52cf95cfef0c6b252cc2035f0c1a7e58a3f2c
Braito <b.riverah@uniandes.edu.co>
2022-11-17 01:23:21 -0500

    como estaba

--- avanzo/settings.py
+++ avanzo/settings.py
@@ -137,8 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+MEDIA_URL = '/static/media/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
