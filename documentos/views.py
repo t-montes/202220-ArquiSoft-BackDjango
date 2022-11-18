f754d46cee41a1a179def6e1305cd72b2477d38b
Your Name <you@example.com>
2022-11-18 16:17:56 -0500

    tipo de la imagen

--- documentos/views.py
+++ documentos/views.py
@@ -7,7 +7,10 @@ from django.http import HttpResponse
 def analizar_documentos(request):
     print (request)
     if request.method == 'POST':
+        print("DOC FILE",request.FILES['docfile'])
         img = request.FILES['docfile']
+        print("DOC FILE . file",img.file)
+
         analizador_documentos(img.file)
         messages.success(request, 'Documento enviado a analizar')
     else:
