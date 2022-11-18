c4a66f615bcee0b2b9dc6d18cf53285e573159ee
Your Name <you@example.com>
2022-11-18 15:16:16 -0500

    tipo de la imagen

--- documentos/views.py
+++ documentos/views.py
@@ -3,14 +3,16 @@ from .logic.documentos_logic import analizador_documentos
 from django.contrib import messages
 from .forms import DocumentosForm
 from django.http import HttpResponse
-
-
+import io
+from PIL import Image
 
 def analizar_documentos(request):
     print (request)
     if request.method == 'POST':
         img = request.FILES['docfile']
         print("tipo", type(img.file))
+        aux_im = Image.open(io.BytesIO((img.file).content))
+        print("tipo", type(aux_im))
         analizador_documentos(img)
         messages.success(request, 'Documento enviado a analizar')
     else:
