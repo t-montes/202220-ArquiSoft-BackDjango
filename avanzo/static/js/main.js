cfa43f0aa45a752e9a87ff7bac9723c06dd880f5
t-montes <t.montes@uniandes.edu.co>
2022-11-19 01:13:32 -0500

    change redirect - create credito

--- avanzo/static/js/main.js
+++ avanzo/static/js/main.js
@@ -27,5 +27,6 @@ if (form_creditos != null) {
         var form_data_json = JSON.stringify(Object.fromEntries(form_data));
         console.log(form_data_json);
         localStorage.setItem('credit_create_data', form_data_json);
+        window.location.href = "/creditos/";
     });
 }
