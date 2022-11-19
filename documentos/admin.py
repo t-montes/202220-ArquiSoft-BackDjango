61c256c7f65dfc0908044a42b5855a6d89f091d3
Braito <b.riverah@uniandes.edu.co>
2022-11-18 22:31:23 -0500

    a

--- documentos/admin.py
+++ documentos/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Documento
 class DocumentoAdmin(admin.ModelAdmin):
-    readonly_fields = ('id')
+    readonly_fields = ('id',)
 
 admin.site.register(Documento, DocumentoAdmin)
