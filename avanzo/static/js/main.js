ca0dfb80bdb6a266db4b28a53b8bc1dd87daa000
t-montes <t.montes@uniandes.edu.co>
2022-11-19 02:22:06 -0500

    little js debug changes

--- avanzo/static/js/main.js
+++ avanzo/static/js/main.js
@@ -12,17 +12,24 @@ if (switch_bool == null) {
 }
 console.log("switch bool = " + switch_bool);
 
-// FORMULARIO DE CREDITOS
+// CAMBIAR SIGNO DE PESOS
+let x = document.querySelectorAll(".money");
+for (let i = 0, len = x.length; i < len; i++) {
+    let num = Number(x[i].innerHTML)
+              .toLocaleString('en');
+    x[i].innerHTML = num;
+    x[i].classList.add("currSign");
+}
 
+// FORMULARIO DE CREDITOS
 var form_creditos = document.getElementById("form-create-credito");
 if (form_creditos != null) {
     // recuperar datos del formulario
     // incluso si el formulario no se envía (es decir, no hay conexión) se guardan los datos
     form_creditos.addEventListener("submit", (e) => {
         var form_data = new FormData(form_creditos);
-        var form_data_json = JSON.stringify(Object.fromEntries(form_data));
-        console.log("Datos almacenados",form_data_json);
-        localStorage.setItem('credit_create_data', form_data_json); // guardar datos en memoria local
+        console.log("Datos almacenados",form_data);
+        localStorage.setItem('credit_create_data', form_data); // guardar datos en memoria local
     });
 }
 
@@ -31,14 +38,13 @@ var credit_create_data = localStorage.getItem('credit_create_data');
 if (credit_create_data != null) {
     // enviar petición POST a /creditos/creditocreate
     console.log("Se recuperaron datos para enviar", credit_create_data);
-    var xhr = new XMLHttpRequest();
+    var xhr = new window.XMLHttpRequest();
     xhr.open("POST", "/creditos/creditocreate", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     resp = xhr.send(credit_create_data);
     console.log("Crédito recuperado y enviado");
     console.log(resp);
     // eliminar datos del formulario
-    localStorage.removeItem('credit_create_data');
 } else {
     console.log("No hay datos almacenados para enviar");
 }
