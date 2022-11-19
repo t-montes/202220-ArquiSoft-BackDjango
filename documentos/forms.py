4fd302b698ce55837d587819cbee78ef421010b6
Braito <b.riverah@uniandes.edu.co>
2022-11-19 02:43:30 -0500

    update credito create and roles

--- documentos/forms.py
+++ documentos/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Documento
 
 class DocumentosForm(forms.ModelForm):
-    docfile = forms.FileField(label='Seleccione el archivo')
     class Meta:
         model = Documento
         fields = "__all__"
