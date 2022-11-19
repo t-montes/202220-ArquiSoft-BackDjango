90c08a5f74fab0ef439251b67a603eb2fb9e0304
t-montes <t.montes@uniandes.edu.co>
2022-11-19 02:04:37 -0500

    little js debug changes

--- avanzo/static/js/main.js
+++ avanzo/static/js/main.js
@@ -12,21 +12,32 @@ if (switch_bool == null) {
 }
 console.log("switch bool = " + switch_bool);
 
-btn_example = document.getElementById("btn-example");
-if (btn_example != null) {
-    btn_example.addEventListener("click", () => { console.log("example clicked"); });
-}
-
 // FORMULARIO DE CREDITOS
 
 var form_creditos = document.getElementById("form-create-credito");
 if (form_creditos != null) {
+    // recuperar datos del formulario
+    // incluso si el formulario no se envía (es decir, no hay conexión) se guardan los datos
     form_creditos.addEventListener("submit", (e) => {
-        // e.preventDefault();
-        console.log(form_creditos);
         var form_data = new FormData(form_creditos);
-        console.log(form_data);
         var form_data_json = JSON.stringify(Object.fromEntries(form_data));
-        localStorage.setItem('credit_create_data', form_data_json);
+        console.log("Datos almacenados",form_data_json);
+        localStorage.setItem('credit_create_data', form_data_json); // guardar datos en memoria local
     });
+}
+
+// Recuperar datos del formulario
+var credit_create_data = localStorage.getItem('credit_create_data');
+if (credit_create_data != null) {
+    // enviar petición POST a /creditos/creditocreate
+    console.log("Se recuperaron datos para enviar", credit_create_data);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/creditos/creditocreate", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(credit_create_data);
+    console.log("Crédito recuperado y enviado");
+    // eliminar datos del formulario
+    localStorage.removeItem('credit_create_data');
+} else {
+    console.log("No hay datos almacenados para enviar");
 }
