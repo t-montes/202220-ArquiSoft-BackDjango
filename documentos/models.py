c7e0fa69ac9a8d830e018b1a97ddb54d3ac901d4
Braito <b.riverah@uniandes.edu.co>
2022-11-19 00:00:55 -0500

    update credito create and roles

--- documentos/models.py
+++ documentos/models.py
@@ -4,4 +4,4 @@ class Documento(models.Model):
     path_image = models.CharField(max_length=100)
     num_documento = models.CharField(max_length=100)
     nombre = models.CharField(max_length=100)
-    imagen = models.ImageField(upload_to='Proyecto_Uno/static/media')
+    imagen = models.ImageField(upload_to='Proyecto_Uno/avanzo/static/media')
