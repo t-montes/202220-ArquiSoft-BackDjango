ad4a612f9d61930df5159378fbdc07c2db1fb7ab
Braito <b.riverah@uniandes.edu.co>
2022-11-19 04:30:00 -0500

    update credito create and roles

--- documentos/forms.py
+++ documentos/forms.py
@@ -4,15 +4,4 @@ from .models import Documento
 class DocumentosForm(forms.ModelForm):
     class Meta:
         model = Documento
-        fields = [
-            'nombre',
-            'num_documento',
-            'path_image',
-            'imagen'
-        ]
-        labels = {
-            'nombre': 'Nombre',
-            'num_documento': 'Num_documento',
-            'path_image': 'Path_image',
-            'imagen': 'Imagen'
-        }
+        fields = "__all__"
