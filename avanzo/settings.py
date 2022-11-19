b7447011c6e4598c44dc5b62019493a57652d885
Braito <b.riverah@uniandes.edu.co>
2022-11-19 10:51:15 -0500

    update credito create and roles

--- avanzo/settings.py
+++ avanzo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*_+g!=9x=jfg86plx47c$c*q63cfy1rqchqas(k38gx$=0u6rp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
