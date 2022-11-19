dcd6562da63ab12f9d9ed6226f6ae891beeec5e6
t-montes <t.montes@uniandes.edu.co>
2022-11-19 04:40:05 -0500

    little js debug changes

--- creditos/views.py
+++ creditos/views.py
@@ -86,13 +86,11 @@ def credito_create(request):
                 body = json.loads(request.body)
                 form.instance.monto = float(body['monto'])
                 form.instance.cuotas = int(body['cuotas'])
-                form.instance.estado = "PENDIENTE"
                 form.instance.csrfmiddlewaretoken = body['csrfmiddlewaretoken']
                 form.clean()
 
             print("FORM",form)
             print("dir-form",dir(form))
-            print("cleaned-data",form.cleaned_data)
             print("form.instance",form.instance)
             if form.is_valid():
                 print("[VIEWS] form valid")
