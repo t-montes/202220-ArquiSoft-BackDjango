82a0102db0479a1c80ab4970bc809d3509695b6d
t-montes <t.montes@uniandes.edu.co>
2022-11-19 01:12:01 -0500

    change 500

--- avanzo/static/js/main.js
+++ avanzo/static/js/main.js
@@ -18,17 +18,14 @@ if (btn_example != null) {
 }
 
 // FORMULARIO DE CREDITOS
-var form_submit_btn = document.getElementById("form-submit-btn");
-if (form_submit_btn != null) {
-    form_submit_btn.addEventListener("click", () => {
-        var form = document.getElementById("form-creditos");
-        console.log("Form:", form);
-        var form_data = new FormData(form);
+
+var form_creditos = document.getElementById("form-create-credito");
+if (form_creditos != null) {
+    form_creditos.addEventListener("submit", (e) => {
+        e.preventDefault();
+        var form_data = new FormData(form_creditos);
         var form_data_json = JSON.stringify(Object.fromEntries(form_data));
-        console.log("Credito Form",form_data_json);
-        /*var xhr = new XMLHttpRequest();
-        xhr.open("POST", "/creditos", true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(form_data_json);*/
+        console.log(form_data_json);
+        localStorage.setItem('credit_create_data', form_data_json);
     });
 }
